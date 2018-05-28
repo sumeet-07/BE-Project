e4cd383bf7c2215bbdb8b6fce6b1f777da26c173
Sumeet Patond <sumeetpatond@gmail.com>
2018-05-29 00:34:53 +0530

    first commit

--- cleanup.sh
+++ cleanup.sh
@@ -3,3 +3,7 @@ docker network prune
 docker container prune
 docker image prune
 docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi
+docker stack rm sassapp
+docker stack rm sassbank
+
+
