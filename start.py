12d1eafd524a28dc7a3f69532dd1bab9613f4640
Sumeet Patond <sumeetpatond@gmail.com>
2018-05-28 23:23:22 +0530

    first commit

--- start.py
+++ start.py
@@ -3,48 +3,58 @@ import time
 import schedule
 
 def init():
+	print "##### Creating Database Directories #####"
 	os.system('mkdir sassapp_mongodbdata')
 	os.system('mkdir sassapp_mysqldata')
 	os.system('mkdir sassbank_mysqldata')
+	print "##### Creating Database Directories Finished #####"
+	print ""
+	print "##### Creating Docker Swarm #####"
 	os.system('docker swarm init')	
+	print "##### Creating Docker Swarm Finished #####"
 
 def deploy_stack(filename,stackname):
+	print ""
+	print "##### Deploying "+stackname+" stack #####"
 	os.system("docker stack deploy -c "+filename+" "+stackname)
+	print "##### Deploying "+stackname+" stack finished #####"
 
 def remove_stacks():
+	print ""
+	print "##### Removing Stacks #####"
 	os.system('docker stack rm sassapp')
 	os.system('docker stack rm sassbank')
-	time.sleep(5)	
+	time.sleep(10)	
+	print "##### Removing Stacks Finished #####"
 	
 
-def job1():
-	print "Working hours"
+def config1():
 	remove_stacks()
+	print ""
+	print "##### Deploying working hours stack #####"
 	deploy_stack("bank-config1.yml","sassbank")
 	deploy_stack("app-config1.yml","sassapp")
+	print "##### Deploying working hours stack Finished #####"
 
-def job2():
-	print "Non-working hours"
+def config2():
 	remove_stacks()
+	print ""
+	print "##### Deploying non-working hours stack #####"
 	deploy_stack("bank-config2.yml","sassbank")
 	deploy_stack("app-config2.yml","sassapp")
-
+	print "##### Deploying non-working hours stack Finished #####"
 
 if __name__ == "__main__":
 
 	t= time.localtime()
 
 	if t.tm_hour>10 and t.tm_hour<17:
-		print "Working hours"
-		deploy_stack("bank-config1.yml","sassbank")
-		deploy_stack("app-config1.yml","sassapp")
+		config1()
 	else:
-		print "Non-working hours"
-		deploy_stack("bank-config2.yml","sassbank")
-		deploy_stack("app-config2.yml","sassapp")
+		config2()
 
-	schedule.every().day.at("10:00").do(job1)
-	schedule.every().day.at("17:00").do(job2)
+	schedule.every().day.at("10:00").do(config1)
+	schedule.every().day.at("17:00").do(config2)
 
 	while 1:
 	    schedule.run_pending()
